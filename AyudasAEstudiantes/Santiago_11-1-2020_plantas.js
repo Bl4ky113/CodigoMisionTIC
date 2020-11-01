dbad896168e6fccff57d3241f342a2a7ffd6ee3a
Bl4ky113 <martinstevenhernandezortiz@gmail.com>
2020-11-01 15:54:35 -0500

    Codigo ahora mas legible

--- AyudasAEstudiantes/Santiago_11-1-2020_plantas.js
+++ AyudasAEstudiantes/Santiago_11-1-2020_plantas.js
@@ -1,46 +1,53 @@
-﻿# Modified By Bl4ky113
+﻿//Modified By Bl4ky113
 document.write("<h1><center>Este programa calcula el promedio de días que debe estar una planta en un invernadero</h1></center>");
-var nPlantas=1;var promedioD=0;
-var crecimiento,Tmaximo,tasa;
+var nPlantas = 1; var promedioD = 0;
+var crecimiento, Tmaximo, tasa;
 do{
-  var fmla=parseInt(prompt("Inserte la familia a la que pertenece la planta(1-3)"));
-  if(fmla==1||fmla==2||fmla==3){
-    var Tinicial=parseFloat(prompt("Inserte el tamaño de la planta(en metros)"));
-    if(fmla==1&&Tinicial<20||fmla==2&&Tinicial<100||fmla==3&&Tinicial<4){
+  var fmla = parseInt(prompt("Inserte la familia a la que pertenece la planta(1-3)"));
+  if(fmla == 1 || fmla == 2 || fmla == 3 ){
+    var altPlntInicio = parseFloat(prompt("Inserte el tamaño de la planta(en metros)"));
+    if(fmla == 1 && altPlntInicio<20 || fmla == 2 && Tinicial < 100 || fmla == 3 && Tinicial < 4){
       switch(fmla){
         case 1:
-          Tmaximo=20;tasa=0,02;break;
+          Tmaximo = 20;
+          tasa = 0,02;
 
+        break;
         case 2:
-          Tmaximo=100;tasa=0,1;break;
+          Tmaximo = 100;
+          tasa = 0,1;
 
+        break;
         case 3:
-          Tmaximo=4;tasa=0,03;break;
+          Tmaximo = 4;
+          tasa = 0,03;
 
+        break;
         default:
           document.write("<h3>Error</h3>")
 
+        break;
       }
     nPlantas++;
 
     } else{
-      var Tinicial=parseFloat(prompt("Inserte el tamaño de la planta(en metros)"));
-      var fmla=parseInt(prompt("Inserte la familia a la que pertenece la planta(1-3)"));
+      var Tinicial = parseFloat(prompt("Inserte el tamaño de la planta(en metros)"));
+      var fmla = parseInt(prompt("Inserte la familia a la que pertenece la planta(1-3)"));
 
       }
   } else{
-      var fmla=parseInt(prompt("Inserte la familia a la que pertenece la planta(1-3)"));
+      var fmla = parseInt(prompt("Inserte la familia a la que pertenece la planta(1-3)"));
 
     }
   do{
-    crecimiento=(Tinicial+(Tinicial*tasa));
+    crecimiento = (Tinicial+(Tinicial*tasa));
     promedioD++;
 
-  } while(crecimiento<=Tmaximo){
+  } while(crecimiento <= Tmaximo){ //Este While no se puede usar
     document.write("<br",promedioD,"</br>")
 
     }
-} while(nPlantas<=2){
+} while(nPlantas <= 2){ //Este While no se puede usar
 
   }
 
@@ -49,14 +56,25 @@ var c2 = parseInt(promp("(1)Amarillo,(2)Azul(3)Rojo y (4)Blanco"));
 var combinacion
 switch(c1){
     case 1:
-        ;break;
+      ;
+
+    break;
     case 2:
-       ; break;
+      ;
+
+    break;
     case 3:
-       ; break;
+      ;
+
+    break;
     case 4:
-        ;break;
+      ;
+
+    break;
     default:
-        document.write("X")
+      document.write("X")
+    break;
+
 }
+
 document.write("El resultado de la combinación es",)
