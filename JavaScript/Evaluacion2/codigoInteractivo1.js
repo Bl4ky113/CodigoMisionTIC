a089fc668b282783468ca4b2920fa0bcb8c3c377
Bl4ky113 <martinstevenhernandezortiz@gmail.com>
2020-11-02 17:19:17 -0500

    Agregado las variables para hacer el switch

--- JavaScript/Evaluacion2/codigoInteractivo1.js
+++ JavaScript/Evaluacion2/codigoInteractivo1.js
@@ -1,1 +1,30 @@
 //Made By Bl4ky1113
+
+//Variables
+
+var num;
+
+num = prompt("Ingrese un número");
+
+var lengNum = num.lengt;
+
+//Function
+
+switch (lengNum) {
+  case 1:
+    document.write("")
+  break;
+  case 2:
+    document.write("")
+  break;
+  case 3:
+    document.write("")
+  break;
+  case 4:
+    document.write("")
+  break;
+  default:
+    document.write("")
+  break;
+
+}
