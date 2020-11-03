3a3640f6c9c2a088066951cb34b5bd0b9907941e
Bl4ky113 <martinstevenhernandezortiz@gmail.com>
2020-11-03 12:57:48 -0500

    Se agrego las variables y la funcion del codigo

--- JavaScript/Evaluacion2/codigoInteractivo3.js
+++ JavaScript/Evaluacion2/codigoInteractivo3.js
@@ -1,1 +1,18 @@
 //Made By Bl4ky1113
+
+//Variables
+
+var num1, num2, num3;
+
+num1 = parseInt(prompt("Ingrese un número"));
+num2 = parseInt(prompt("Ingrese otro número"));
+num3 = parseInt(prompt("Ingrese el ultimo número"));
+
+//Function
+
+if (num1 > num2 && num2 > num3 || num3 > num2 && num2 > num1) {
+  document.write("Los números que ingreso, se ingresaron de una forma ordenada");
+
+} else {
+  document.write("Los números que ingreso, se ingresaron de una forma desordenada");
+}
