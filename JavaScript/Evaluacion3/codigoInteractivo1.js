5f3ece09eaab4e035da0ef53dee655650b9c3713
Bl4ky113 <martinstevenhernandezortiz@gmail.com>
2020-11-07 15:23:25 -0500

    Agregado todo el codigo

--- JavaScript/Evaluacion3/codigoInteractivo1.js
+++ JavaScript/Evaluacion3/codigoInteractivo1.js
@@ -1,1 +1,13 @@
 //Made By Bl4ky113
+
+var num = 0;
+
+for (i = 0; i <= 100; i++) {
+  num = num + i;
+  console.log(num);
+}
+
+console.log("This wolud be the result:  ", num);
+
+
+//LoL that was a 3 min code XD
