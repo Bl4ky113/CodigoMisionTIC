3713bbaaab984742fa553b6908b2d0f1aa3c5af3
Bl4ky113 <martinstevenhernandezortiz@gmail.com>
2020-11-07 18:22:15 -0500

    Codigo hecho, salida de datos por Document y console

--- JavaScript/Evaluacion3/codigoInteractivo2.js
+++ JavaScript/Evaluacion3/codigoInteractivo2.js
@@ -1,2 +1,24 @@
 //Made By Bl4ky113
-alert("hola");
+
+var results;
+var add = 0;
+var evenNum = [];
+
+for (i = 1;i <= 29; i++) {
+  if (i%2 == 0) {
+    evenNum.push(i);
+    add = add + i;
+    results = {
+      square: i ** 2,
+      cube: i ** 3,
+      average: add / evenNum.length,
+
+    };
+    document.write("The Square of the ", i, " number is  ", results["square"], "  ;and its cube is:  ", results["cube"], ". <br />");
+    console.log("Square: ", results["square"], "Cube:  ", results["cube"])
+  }
+
+}
+
+document.write("The average is:  ", results["average"], " of the ", evenNum.length, " numbers.")
+console.log("Average:  ", results["average"]);
