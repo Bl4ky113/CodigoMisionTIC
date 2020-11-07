956ef6a18a3cb0c64157091f2eefb89f44c1156c
Bl4ky113 <martinstevenhernandezortiz@gmail.com>
2020-11-07 18:20:24 -0500

    Salida de datos por Document y Console.log

--- JavaScript/Evaluacion3/codigoInteractivo1.js
+++ JavaScript/Evaluacion3/codigoInteractivo1.js
@@ -5,9 +5,11 @@ var num = 0;
 for (i = 0; i <= 100; i++) {
   num = num + i;
   console.log(num);
+  document.write("Result number ", i, " :   ", num, "<br />");
 }
 
 console.log("This wolud be the result:  ", num);
+document.write("Final Result is:  ", num);
 
 
 //LoL that was a 3 min code XD
