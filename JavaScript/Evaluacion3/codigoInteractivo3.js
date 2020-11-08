65a37b07777b0b4407cbf3feabb88667a7f09440
Bl4ky113 <martinstevenhernandezortiz@gmail.com>
2020-11-07 19:13:12 -0500

    codigo terminado

--- JavaScript/Evaluacion3/codigoInteractivo3.js
+++ JavaScript/Evaluacion3/codigoInteractivo3.js
@@ -1,3 +1,14 @@
 //Made By Bl4ky113
 
-alert("Hello");
+//Variables
+
+var results;
+
+//Function
+
+for (i = 1; i <= 20; i++) {
+  results = i ** 2;
+  console.log("results", results);
+  document.write("The squeare of the number ", i, " is:  ", results, ". <br />");
+
+}
