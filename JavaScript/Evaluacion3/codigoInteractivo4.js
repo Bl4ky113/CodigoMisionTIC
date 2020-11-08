bf480f34d70b73238525eab7fa71dc985302982c
Bl4ky113 <martinstevenhernandezortiz@gmail.com>
2020-11-08 14:39:12 -0500

    Primer commit, variables establecidas

---
+++ JavaScript/Evaluacion3/codigoInteractivo4.js
@@ -0,0 +1,13 @@
+//Made By Bl4ky113
+
+//Variables
+
+var data = {
+  product: prompt("Ingrese el nombre del producto vendido: "),
+  price: parseInt(prompt("Ingrese el precio del producto vendido: ")),
+  amount: parseInt(prompt("Ingrese la cantidad de productos vendidos")),
+  quantitySellers: parseInt(prompt("Ingrese el número de vendedores en la venta: ")),
+
+};
+
+console.log(data);
