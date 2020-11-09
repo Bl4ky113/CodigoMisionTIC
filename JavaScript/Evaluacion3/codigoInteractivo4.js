2b6a8982446ffcd1b345938c539d4b665a6a4dd1
Bl4ky113 <martinstevenhernandezortiz@gmail.com>
2020-11-09 17:35:49 -0500

    codigo terminado, con function para mejor funcionalidad

--- JavaScript/Evaluacion3/codigoInteractivo4.js
+++ JavaScript/Evaluacion3/codigoInteractivo4.js
@@ -2,12 +2,47 @@
 
 //Variables
 
+var profit;
+
 var data = {
   product: prompt("Ingrese el nombre del producto vendido: "),
   price: parseInt(prompt("Ingrese el precio del producto vendido: ")),
   amount: parseInt(prompt("Ingrese la cantidad de productos vendidos")),
-  quantitySellers: parseInt(prompt("Ingrese el número de vendedores en la venta: ")),
+  numSellers: parseInt(prompt("Ingrese el número de vendedores en la venta: ")),
 
 };
 
-console.log(data);
+var percentage = [
+  0.025, //If product price is less than 100k
+  0.1,  //If product price is equal or greater than 100k
+
+];
+
+var total = data["price"] * data["amount"];
+
+//Function
+
+document.write("Producto:  ", data["product"], "<br />");
+document.write("Precio:  ", data["price"], "<br />");
+document.write("Cantidad:  ", data["amount"], "<br />");
+document.write("Cantidad de Vendedores:  ", data["numSellers"], "<br />");
+document.write("Total Venta:  ", total, "<br /><br />");
+
+if (data["price"] < 100000) {
+  var payment = total * percentage[0];
+  var profit = total - (payment * data["numSellers"]);
+  sellersCommission(payment);
+
+} else if (data["price"] >= 100000) {
+  var payment = total * percentage[1];
+  var profit = total - (payment * data["numSellers"]);
+  sellersCommission(payment);
+
+}
+function sellersCommission (payCommission) {
+  for (i = 1; i <= data["numSellers"]; i++) {
+    document.write("La paga del vendedor número ", i, " es de:  ", payCommission, ". <br />");
+
+  }
+  document.write("<br />Ganancias venta:  ", profit);
+}
