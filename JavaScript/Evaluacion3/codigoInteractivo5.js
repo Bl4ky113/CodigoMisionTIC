2cbbbaeac83435be219661939c307aba195b3b64
Bl4ky113 <martinstevenhernandezortiz@gmail.com>
2020-11-09 15:15:59 -0500

    Codigo Terminado

--- JavaScript/Evaluacion3/codigoInteractivo5.js
+++ JavaScript/Evaluacion3/codigoInteractivo5.js
@@ -7,8 +7,21 @@ var num = 1;
 
 //Function
 
+document.write("<h1> Tabla del ", 1, ". </h1> <br />")
+
 for (factor = 1; factor <= 10; factor++) {
   var result = num * factor;
-  console.log(result);
+
+  //Print results
+
+  console.log("Result: ", result, "; factor: ", factor);
+  document.write(num, " X ", factor, " = ", result, ". <br />");
+
+  if (factor == 10 && num < 10){
+    document.write("<h1> Tabla del ", num + 1, ". </h1> <br />");
+    num = num + 1;
+    factor = 0;
+
+  }
 
 }
