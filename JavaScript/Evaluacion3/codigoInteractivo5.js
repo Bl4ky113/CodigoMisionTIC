9bad78f7d258ad315297632e1473a78594ccf5f1
Bl4ky113 <martinstevenhernandezortiz@gmail.com>
2020-11-09 14:27:58 -0500

    primer commit, Variables y for de Factor

---
+++ JavaScript/Evaluacion3/codigoInteractivo5.js
@@ -0,0 +1,14 @@
+//Made By Bl4ky113
+
+//Variables
+
+var factor;
+var num = 1;
+
+//Function
+
+for (factor = 1; factor <= 10; factor++) {
+  var result = num * factor;
+  console.log(result);
+
+}
